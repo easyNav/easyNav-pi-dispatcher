72113f03b9172a2c2525a94b98a8127718090aaa
Joel Tong <me@joeltong.org>
2014-09-21 19:54:42 +0800

    first commit

---
+++ easyNav_pi_dispatcher/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of easyNav-pi-dispatcher.
+# https://github.com/easyNav/easyNav-pi-dispatcher
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Joel Tong me@joeltong.org
+
+from easyNav_pi_dispatcher.version import __version__
